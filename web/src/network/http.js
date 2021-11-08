6cb55641c47b15af3b85c0e1bca400aeb58db570
xiduoshi-glb <xiduoshi-glb@qq.com>
2021-11-08 21:05:51 +0800

    main

--- web/src/network/http.js
+++ web/src/network/http.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const http = axios.create({
-    baseURL: 'http://localhost:3000/web/api',
+    // baseURL: 'http://localhost:3000/web/api',
     // VUE_APP_API_URL 传了就用本身 否则用域名下的
-    // baseURL: process.env.VUE_APP_API_URL || '/web/api'
+    baseURL: process.env.VUE_APP_API_URL || '/web/api'
 });
 export default http
